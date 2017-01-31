f4fe579b3f54a61824aee13dbbbf5331755eca50
sreesanpd <sreesanpd@yahoo.co.in>
2017-02-01 01:15:30 +0530

    program to rename files

---
+++ rename_files.py
@@ -0,0 +1,15 @@
+import os
+
+def rename_files():
+	# (1) get file names from a folder
+	file_list = os.listdir("/home/sreesan/Projects/Python/secret_message/prank/")
+	print(file_list)
+	saved_path = os.getcwd()
+	print ("Current Working Directory is: "+saved_path)
+	os.chdir("/home/sreesan/Projects/Python/secret_message/prank/")
+	# (2) for each file, rename filename
+	for file_name in file_list:
+		os.rename(file_name, file_name.lstrip("0123456789"))
+	os.chdir(saved_path)
+
+rename_files()
